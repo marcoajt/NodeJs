6c02a499aeae79dab43f3e07cf6e7a8824aa5b34
marcoajt <marcoantoniojt@gmail.com>
2020-08-16 13:50:01 -0300

    Body-parser

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const Sequelize = require('sequelize')
 
 
 
@@ -10,8 +12,11 @@ const handlebars = require('express-handlebars')
     app.engine('handlebars',handlebars({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
 
+  //Body Parser 
+    app.use(bodyParser.urlencoded({extended: false}))
+    app.use(bodyParser.json())
+
   //Conexão com o banco de dados MySql
-    const Sequelize = require('sequelize')
     const sequelize = new Sequelize('test', 'root', 'marco04', {
       host: "localhost",
       dialect: 'mysql'
@@ -23,7 +28,7 @@ const handlebars = require('express-handlebars')
     })
 
     app.post('/add', function(req,res){
-      res.send('FORMULARIO RECEBIDO')
+      res.send("Texto: "+req.body.titulo+" Conteudo: "+req.body.conteudo)
     })
 
 app.listen(8081, function(){
